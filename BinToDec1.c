ef66eba0fb57a2edf68634685ba4692b0454fd34
VishalM97 <ucs15326@rmd.ac.in>
2018-03-01 18:46:17 +0530

    Binary to Decimal without %

---
+++ BinToDec1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main()
+{
+    long long int num;
+    int count=0;
+    scanf("%lld", &num);
+    while(num>0)
+    {
+        if(num/2!=(float)num/2)
+            count++;
+        num/=2;
+    }
+    printf("%d", count);
+}
