ebf74578e420640d1ff945563430bf275407c6c5
VishalM97 <ucs15326@rmd.ac.in>
2018-03-04 13:23:23 +0530

    Updated Number of Occurrences of Numbers

--- NoOfOccNums.c
+++ NoOfOccNums.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 int main()
 {
-    int arr[1000], n=-1, i, j, count;
-    while(scanf("%d", &arr[++n])!=-1)
-        ;
+    int arr[1000][2], n=0, i, j, count;
+    while(scanf("%d", &arr[n][0])!=-1)
+    {    
+        arr[n++][1]=0;
+    }
     for(i=0;i<n;i++)
     {
-        if(arr[i]!=-1)
+        if(!arr[i][1])
         {
             for(j=i+1, count=1;j<n;j++)
             {
-                if(arr[i]==arr[j])
+                if(arr[i][0]==arr[j][0])
                 {
                     count++;
-                    arr[j]=-1;
+                    arr[j][1]=1;
                 }
             }
-            printf("%d --> %d\n", arr[i], count);
+            printf("%d --> %d\n", arr[i][0], count);
         }
     }
 }
